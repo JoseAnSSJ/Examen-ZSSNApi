dd7913085f3cf4f834be422f16ffe00069e463fd
Jose Antonio Garcia Rodriguez <joseantonio.garcia.rdz@gmail.com>
2022-07-19 19:28:29 -0500

    [FEATURE] Inicio de backend

---
+++ src/main/kotlin/com/examen/zssnapi/ZssnApiApplication.kt
@@ -0,0 +1,11 @@
+package com.examen.zssnapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ZssnApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<ZssnApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/examen/zssnapi/ZssnApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.examen.zssnapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ZssnApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
